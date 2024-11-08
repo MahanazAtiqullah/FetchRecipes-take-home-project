4417d14b531862f4754eaefe4925ac44bba77f4c
Mahanaz Atiqullah <132308047+MHA180004@users.noreply.github.com>
2024-11-08 16:36:15 -0600

    Project Initiation

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Mahanaz Atiqullah on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
